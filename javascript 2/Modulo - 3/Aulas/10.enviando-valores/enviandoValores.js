228c18d1f5812ff160959923baec18675798c5fa
Cauã Tavares <cauaxd2010@outlook.com>
2025-12-25 10:36:29 +0000

    fim da aula 11

--- javascript 2/Modulo - 3/Aulas/10.enviando-valores/enviandoValores.js
+++ javascript 2/Modulo - 3/Aulas/10.enviando-valores/enviandoValores.js
@@ -1,7 +1,21 @@
 function register(element) {
     const username = element.children.username.value
+    // element é o ELEMENTO que está vindo como parâmetro
+    // .children é os filhos do elemento
+    // .username é o (ID) do INPUT
+    // .value pega o valor do INPUT
+
     const password = element.children.password.value
+    // element é o ELEMENTO que está vindo como parâmetro
+    // .children é os filhos do elemento
+    // .password é o (ID) do INPUT
+    // .value pega o valor do INPUT
+
     const passwordConfirmation = element.children.passwordConfirmation.value
+    // element é o ELEMENTO que está vindo como parâmetro
+    // .children é os filhos do elemento
+    // .passwordConfirmation é o (ID) do INPUT
+    // .value pega o valor do INPUT
 
     console.log(username, password, passwordConfirmation);
 
---
+++ javascript 2/Modulo - 3/Aulas/11.trabalhando-com-eventos/trabalhandoEventos.js
@@ -0,0 +1,38 @@
+//---------------------------------------------------------ADICIONAR EVENTO
+
+function register(ev) {
+    console.log(ev);
+    const sectionElement = ev.currentTarget.parentNode
+    // Acessou o evento(ev)
+    // Pegou o alvo do evento(Target)
+    // Pegou o NÓ pai(parentNode)
+
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+        alert('Usuário ' + username + ' registrado!')
+    } else {
+        alert('As senhas não conferem!')
+    }
+}
+//---------------------------------------------------------ADICIONAR EVENTO
+
+const button = document.getElementById('register-button')   // Serve para pegar o BOTÃO pelo (ID)
+
+button.addEventListener('click', register)  // Passar a função sem () para NÃO pegar os resultados
+// Serve para adicionar no botão uma funcionalidade no botão para ele ficar esperando esses EVENTOS acontecerem 
+
+//--------------------------------------------------------REMOVER EVENTO
+
+function removeListener() {
+    button.removeEventListener('click', register)   // VARIÁVEL e informar qual evento quer remover
+    alert('Event removed')
+}
+
+//----------------------------------------------------------ADICIONAR EVENTO
+
+button.addEventListener('mouseover', function (ev) {
+    console.log(ev.currentTarget);
+})
